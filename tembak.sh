7ba6bbdc2539286f1af9372d9bc0f8c585b38a53
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-25 17:50:30 +0700

    Create tembak.sh

---
+++ tembak.sh
@@ -0,0 +1,39 @@
+figlet MR_XID  | lolcat
+
+echo                    " [======<>======<>======<>======]"|lolcat
+echo                    " [Author   :  MR_XiD            ]"|lolcat
+echo                    " [Team     : JATIM BLACKHAT     ]"|lolcat
+echo                    " [Note     : Jangan reqode ya   ]"|lolcat
+echo                    " [Thansk to: All member         ]"|lolcat
+echo                    " [thanks to: I T A              ]"|lolcat
+echo                    " [Note2    : Copyright©MR_XID   ]"|lolcat
+echo                    " [======<>======<>======<>======]"|lolcat
+
+echo -e"---_-------------_---_--------" | lolcat
+echo -e $b "1.  TEMBAK CEWE${endc}";
+echo -e"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" | lolcat
+echo -e "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" saf;
+
+# TEMBAK CEWE
+case $saf in
+(1)
+echo -e "----------------------"|lolcat
+echo -e $r "1.cara tembak cewe ${endc}";
+echo -e "----------------------"|lolcat
+echo -e "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" saf;
+
+case $saf in
+(1)echo "CARA MENEMBAK CEWEK YA TONG 😅" | lolcat
+echo "PERTAMA KAMU HARUS PUNYA DUIT"
+echo "KALO KAGA YAUDAH GAGAL DEH 😅😅
+echo "JOMBLO HAPPY AJA TOD 😂
+exit
+
+;;
+
+*) echo "Tod nomor yang elu pilih kaga ada. coky dulu ya"
+esac
+done
+done
